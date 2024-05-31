72dec2cb8e0be0df34b93fc1f14a8c97f7c4659d
chengzimdl@gmail.com <chengzimdl@gmail.com>
2024-05-31 15:49:28 +0200

    Solve the wandering sliding issue, by set the max walk speed = 100.f in task node.

--- Source/Farmer/Private/SBT_FoxInRangeTaskNode.cpp
+++ Source/Farmer/Private/SBT_FoxInRangeTaskNode.cpp
@@ -4,6 +4,8 @@
 #include "SBT_FoxInRangeTaskNode.h"
 #include "AIController.h"
 #include "BehaviorTree/BlackboardComponent.h"
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
 
 void USBT_FoxInRangeTaskNode::OnGameplayTaskActivated(UGameplayTask& Task)
@@ -21,7 +23,7 @@ EBTNodeResult::Type USBT_FoxInRangeTaskNode::ExecuteTask(UBehaviorTreeComponent&
 	AAIController* MyController = OwnerComp.GetAIOwner();
 	if (MyController == nullptr) return EBTNodeResult::Failed;
 
-	APawn* NPC = MyController->GetPawn();
+	ACharacter* NPC = MyController->GetCharacter();
 	if (NPC == nullptr) return EBTNodeResult::Failed;
 
 	AActor* TargetActor = Cast<AActor>(OwnerComp.GetBlackboardComponent()->GetValueAsObject("TargetActor"));
@@ -29,11 +31,12 @@ EBTNodeResult::Type USBT_FoxInRangeTaskNode::ExecuteTask(UBehaviorTreeComponent&
 
 	FVector EndLocation;
 	EndLocation = GetRandomLocationWithinRange(TargetActor->GetActorLocation(), WanderRange);
-	//MyController->MoveToLocation(EndLocation);
 
 	UBlackboardComponent* BB = OwnerComp.GetBlackboardComponent();
 	if (BB == nullptr) return EBTNodeResult::Failed;
 	BB->SetValueAsVector("MoveToLocation", EndLocation);
+
+	NPC->GetCharacterMovement()->MaxWalkSpeed = 100.f;
 
 	return EBTNodeResult::Succeeded;
 }
