274f40e1e71bfe8dfc7f5a3218e4bb3b24fca120
chengzimdl@gmail.com <chengzimdl@gmail.com>
2024-06-20 15:54:15 +0200

    update

--- Source/Farmer/Private/ResourceTree.cpp
+++ Source/Farmer/Private/ResourceTree.cpp
@@ -34,7 +34,7 @@ void AResourceTree::OnTreeFallComplete()
 	if (WoodClass)
 	{
 		FActorSpawnParameters SpawnParams;
-		GetWorld()->SpawnActor<AActor>(WoodClass, GetActorLocation()-FVector(50,50,50), FRotator::ZeroRotator, SpawnParams);
+		GetWorld()->SpawnActor<AActor>(WoodClass, GetActorLocation()-FVector(50,50,27), FRotator::ZeroRotator, SpawnParams);
 	}		
 	
 	DisappearAndRelease();
