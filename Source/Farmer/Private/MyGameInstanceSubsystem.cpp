671f65dab05e3e4921683bc1fc558688ec4ce49c
chengzimdl@gmail.com <chengzimdl@gmail.com>
2024-12-29 10:24:22 +0100

    added some new assets for testing later.

--- Source/Farmer/Private/MyGameInstanceSubsystem.cpp
+++ Source/Farmer/Private/MyGameInstanceSubsystem.cpp
@@ -8,37 +8,45 @@
 
 
 //void UMyGameInstanceSubsystem::LoadItemAsync(const FPrimaryAssetId& AssetId, TFunction<void(UUItemData*)> OnLoaded)
+//void UMyGameInstanceSubsystem::LoadItemAsync()
+//{
+//	FAssetRegistryModule& AssetRegistry = FModuleManager::LoadModuleChecked<FAssetRegistryModule>("AssetRegistry");
+//	
+//	FStreamableManager& Streamable = UAssetManager::GetStreamableManager();
+//	FSoftObjectPath ObjectPath(FString("C:/Users/cheng/source/repos/UE5/Farmer/Content/BP_Resources/ItemData/Blouse.uasset"));
+//	//FSoftObjectPath ObjectPath(FString("/All/Game/BP_Resources/ItemData"));
+//
+//
+//	FAssetData Asset;
+//	AssetRegistry.TryGetAssetByObjectPath(ObjectPath, Asset);
+//
+//	// async loading
+//	Streamable.RequestAsyncLoad(ObjectPath,
+//		FStreamableDelegate::CreateLambda([&]() 
+//			{
+//				//if (ObjectPath.IsValid()) {
+//					//Asset = ObjectPath.ResolveObject();
+//					//AssetList.Add(Asset);
+//					// TODO: communication/interaction with UI & FarmerCharacter !
+//				//}
+//				if (Asset.IsValid()) {
+//					Asset.GetClass()->AddToRoot(); // Avoid GC
+//					FString AssName = Asset.GetAsset()->GetPrimaryAssetId().ToString();
+//					GEngine->AddOnScreenDebugMessage(-1, INFINITY, FColor::Green,AssName);
+//				}
+//				
+//			})
+//	);
+//
+//
+//	//Asset = ObjectPath.TryLoad();  // this is very slow !!! TODO: remove it once make it work, then use async loading!!!
+//
+//}
+
 void UMyGameInstanceSubsystem::LoadItemAsync()
 {
-	FAssetRegistryModule& AssetRegistry = FModuleManager::LoadModuleChecked<FAssetRegistryModule>("AssetRegistry");
-	
 	FStreamableManager& Streamable = UAssetManager::GetStreamableManager();
-	FSoftObjectPath ObjectPath(FString("C:/Users/cheng/source/repos/UE5/Farmer/Content/BP_Resources/ItemData/Blouse.uasset"));
-	//FSoftObjectPath ObjectPath(FString("/All/Game/BP_Resources/ItemData"));
 
 
-	FAssetData Asset;
-	AssetRegistry.TryGetAssetByObjectPath(ObjectPath, Asset);
-
-	// async loading
-	Streamable.RequestAsyncLoad(ObjectPath,
-		FStreamableDelegate::CreateLambda([&]() 
-			{
-				//if (ObjectPath.IsValid()) {
-					//Asset = ObjectPath.ResolveObject();
-					//AssetList.Add(Asset);
-					// TODO: communication/interaction with UI & FarmerCharacter !
-				//}
-				if (Asset.IsValid()) {
-					Asset.GetClass()->AddToRoot(); // Avoid GC
-					FString AssName = Asset.GetAsset()->GetPrimaryAssetId().ToString();
-					GEngine->AddOnScreenDebugMessage(-1, INFINITY, FColor::Green,AssName);
-				}
-				
-			})
-	);
-
-
-	//Asset = ObjectPath.TryLoad();  // this is very slow !!! TODO: remove it once make it work, then use async loading!!!
 
 }
